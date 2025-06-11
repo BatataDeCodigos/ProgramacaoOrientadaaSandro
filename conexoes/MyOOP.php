aac9f561f4c39a44df9a7f3d07c6597727466c76
FraNFraN <134552777+BatataDeCodigos@users.noreply.github.com>
2025-06-11 08:51:52 -0300

    temos conteúdos

---
+++ conexoes/MyOOP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="oop.css">
+    <title>Informações do jogo</title>
+</head>
+<body>
+<?php
+//conexão com a classe do jogo
+include_once './classes/FNaF.php';
+
+//Instanciando a classe
+$fnaf1 = new FNaF();
+
+//Atributos e suas definições
+$fnaf->nome = "Five Nights At Freddy's 1 ";
+$fnaf->data = 08 . /08 . /2014
+
+
+?>
+</body>
+</html>
